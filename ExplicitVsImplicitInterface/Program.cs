632b86a2bf58547c2b860acc4bf9b54185bc9abe
anjaikr <anjaikr@gmail.com>
2017-08-04 09:59:08 +0530

    ExplicitVsImplicitInterface

--- ExplicitVsImplicitInterface/Program.cs
+++ ExplicitVsImplicitInterface/Program.cs
@@ -6,10 +6,125 @@ using System.Threading.Tasks;
 
 namespace ExplicitVsImplicitInterface
 {
+    //interface Iinterface_1
+    //{
+
+    //}
+    //class Class1 : Iinterface_1
+    //{
+    //    public void Display()
+    //    {
+    //        Console.WriteLine("Class1 Display Method.");
+    //    }
+    //}
+
+    //interface Iinterface_1
+    //{
+    //    void interface1_method();
+    //}
+    //interface Iinterface_2
+    //{
+    //    void interface2_method();
+    //}
+
+    //class Class1 : Iinterface_1, Iinterface_2
+
+    //{
+    //    public void Display()
+    //    {
+    //        Console.WriteLine("Class1 Display Method.");
+    //        //Calling Implicit interface implementation
+    //        interface1_method();
+
+    //        //Call Explicit interface method
+    //        Iinterface_1 obj_1 = new Class1();
+    //        obj_1.interface1_method();
+    //    }
+    //    //Implicit interface implementation
+    //    public void interface1_method()
+    //    {
+    //        Console.WriteLine("Iinterface_1 Method Implicit interface implementation.");
+    //    }
+    //    //Explicit interface implementation
+    //    void Iinterface_1.interface1_method()
+    //    {
+    //        Console.WriteLine("Iinterface_1 Method Explicit interface implementation.");
+
+    //    }
+
+    //    public void interface2_method()
+    //    {
+    //        Console.WriteLine("Iinterface_2 Method.");
+    //    }
+
+    //}
+
+    interface Iinterface_1
+    {
+        void interface1_method();
+    }
+    interface Iinterface_2 : Iinterface_1
+    {
+        void interface2_method();
+    }
+
+    class Class1 : Iinterface_2
+    {
+        public void Display()
+        {
+            Console.WriteLine("Class1 Display Method.");
+        }
+        public void interface1_method()
+        {
+            Console.WriteLine("Iinterface_1 Method Implicit interface implementation.");
+        }
+        public void interface2_method()
+        {
+            Console.WriteLine("Iinterface_2 Method Implicit interface implementation.");
+        }
+        void Iinterface_1.interface1_method()
+        {
+            Console.WriteLine("Iinterface_1 Method Explicit interface implementation.");
+        }
+        void Iinterface_2.interface2_method()
+        {
+            Console.WriteLine("Iinterface_2 Method Explicit interface implementation.");
+        }
+
+    }
+
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            Class1 objClass1 = new Class1();
+            objClass1.Display();
+            //objClass1.interface1_method();
+            //var objIiterface1 = (objClass1) as Iinterface_1;
+            //objClass1.interface1_method();
+
+            ////Call Explicit interface method
+            //Iinterface_1 obj_1 = new Class1();
+            //obj_1.interface1_method();
+
+            //Iinterface_1 obj_1 = new Class1();
+            //obj_1.interface1_method();
+            //Iinterface_2 obj_2 = new Class1();
+            //obj_2.interface2_method();
+            objClass1.interface1_method();
+            objClass1.interface2_method();
+
+            Iinterface_2 obj_2 = new Class1();
+            obj_2.interface1_method();
+            obj_2.interface2_method();
+            //Console.ReadLine();
+
+
+            Console.ReadLine();
         }
     }
+
 }
